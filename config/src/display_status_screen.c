1f206132f668109071341d236f39a42c838f1aa3
KitsuneVog <galchenkovvladislav@gmail.com>
2026-03-24 23:40:34 +0300

    .

--- config/src/display_status_screen.c
+++ config/src/display_status_screen.c
@@ -1,11 +1,11 @@
-#include <lvgl.h>
-#include <zmk/display.h>
+// #include <lvgl.h>
+// #include <zmk/display.h>
 
-lv_obj_t *zmk_display_status_screen() {
-    lv_obj_t *screen = lv_obj_create(NULL); // создаём новый экран
-    lv_obj_t *label = lv_label_create(screen);
-    lv_label_set_text(label, "HELLO 😎");
-    lv_obj_center(label);
+// lv_obj_t *zmk_display_status_screen() {
+//     lv_obj_t *screen = lv_obj_create(NULL); // создаём новый экран
+//     lv_obj_t *label = lv_label_create(screen);
+//     lv_label_set_text(label, "HELLO 😎");
+//     lv_obj_center(label);
 
-    return screen; // возвращаем новый экран
-}
+//     return screen; // возвращаем новый экран
+// }
