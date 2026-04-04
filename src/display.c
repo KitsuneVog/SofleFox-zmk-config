26daba9cb59114bff5fa7abcca05a9ce74f7df4a
KitsuneVog <galchenkovvladislav@gmail.com>
2026-04-04 13:03:15 +0300

    Create display.c

---
+++ src/display.c
@@ -0,0 +1,12 @@
+#include <lvgl.h>
+#include <zmk/display.h>
+
+lv_obj_t *zmk_display_status_screen(void) {
+    lv_obj_t *screen = lv_obj_create(NULL);
+
+    lv_obj_t *label = lv_label_create(screen);
+    lv_label_set_text(label, "HELLO 😎");
+    lv_obj_center(label);
+
+    return screen;
+}
