788c4b858c48a16a84996fbb63c189f5ca245ca0
KitsuneVog <galchenkovvladislav@gmail.com>
2026-04-04 13:13:55 +0300

    Update display.c

--- src/display.c
+++ src/display.c
@@ -2,10 +2,12 @@
 #include <zmk/display.h>
 
 lv_obj_t *zmk_display_status_screen(void) {
-    lv_obj_t *screen = lv_obj_create(NULL);
+    lv_obj_t *screen = lv_scr_act();   // ВАЖНО!
+
+    lv_obj_clean(screen);              // очистить старый UI
 
     lv_obj_t *label = lv_label_create(screen);
-    lv_label_set_text(label, "HELLO 😎");
+    lv_label_set_text(label, "HELLO");
     lv_obj_center(label);
 
     return screen;
